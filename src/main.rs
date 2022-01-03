42f86ad5b893a266e7c95568d0a7e371e377a92c
web3ten0 <web3ten0@gmail.com>
2022-01-04 00:25:43 +0900

    added profile func and openapi-run.yaml

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,46 @@
-use actix_web::{get, App, HttpResponse, HttpServer, Responder};
+use actix_web::{get, App, Error, HttpRequest, HttpResponse, HttpServer, Responder};
+use futures::future::{ready, Ready};
+use serde::Serialize;
 
-#[get("/")]
-async fn index() -> impl Responder {
+#[derive(Serialize)]
+struct User {
+    name: &'static str,
+    age: u16,
+}
+impl Responder for User {
+    type Error = Error;
+    type Future = Ready<Result<HttpResponse, Error>>;
+
+    fn respond_to(self, _req: &HttpRequest) -> Self::Future {
+        let body = serde_json::to_string(&self).unwrap();
+        ready(Ok(HttpResponse::Ok()
+            .content_type("application/json")
+            .body(body)))
+    }
+}
+
+#[get("/hello")]
+async fn hello() -> impl Responder {
     HttpResponse::Ok().body("Hello world!")
+}
+
+#[get("/ping")]
+async fn ping() -> impl Responder {
+    HttpResponse::Ok().body("pong")
+}
+
+#[get("/profile")]
+async fn profile() -> impl Responder {
+    User {
+        name: "taro",
+        age: 30,
+    }
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
-        App::new()
-            .service(index)
-    })
-    .bind("0.0.0.0:8080")?
-    .run()
-    .await
+    HttpServer::new(|| App::new().service(hello).service(ping).service(profile))
+        .bind("0.0.0.0:8080")?
+        .run()
+        .await
 }
